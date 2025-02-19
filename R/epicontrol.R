4ba78bd4594e960a043dc8ebcce5f0cabc271a3d
Sandor Beregi <51128843+sandorberegi@users.noreply.github.com>
2025-02-19 15:53:08 +0000

    betabinom from VGAM

---
+++ R/epicontrol.R
@@ -0,0 +1,42 @@
+#The main constructor function for the simulations
+#Epi_MPC_run_wd <- function(episimdata, epi_par, noise_par, actions, pred_days, n_ens = 100, start_day = 1, ndays = nrow(episimdata), R_est_wind = 5, pathogen = 1, susceptibles = 1, delay = 0, ur = 0, r_dir = 1, N = 1e6) {
+epicontrol <- function(episim_data_ens, settings) {
+  sim_function <- episettings$sim_function
+  projection_function <- episettings$projection_function
+  reward_function <- episettings$reward_function
+  R_estimator <- episettings$R_estimator
+  noise_par <- episettings$noise_par
+  epi_par <- episettings$epi_par
+  actions <- episettings$actions
+  sim_settings <- episettings$sim_settings
+  parallel <- episettings$parallel
+
+  pb <- txtProgressBar(min = 0, max = sim_ens, initial = 0, style = 3, width = 50, char = "=")
+  for (ii in 1:sim_ens) {
+    episim_data_ens[[ii]]$sim_id <- rep(ii, ndays)
+  }
+
+  if (parallel) {
+
+
+    sim_ens <- sim_settings$sim_ens
+
+    results <- pblapply(1:sim_ens, function(idx) {
+      sim_function(episim_data_ens[[idx]], epi_par, noise_par, actions, pred_days = sim_settings$pred_days, n_ens = sim_settings$n_ens, start_day = sim_settings$start_day, ndays = sim_settings$ndays, R_est_wind = sim_settings$R_est_wind, pathogen = sim_settings$pathogen, susceptibles = sim_settings$susceptibles, delay = sim_settings$delay, ur = sim_settings$ur, r_dir = sim_settings$r_dir, N = sim_settings$N)
+    }, cl = cl)
+
+  }
+
+  else {
+    for (jj in 1:sim_ens) {
+      episim_data_ens[[jj]] <- sim_function(episim_data_ens[[jj]], epi_par, noise_par, actions, pred_days = sim_settings$pred_days, n_ens = sim_settings$n_ens, start_day = sim_settings$start_day, ndays = sim_settings$ndays, R_est_wind = sim_settings$R_est_wind, pathogen = sim_settings$pathogen, susceptibles = sim_settings$susceptibles, delay = sim_settings$delay, ur = sim_settings$ur, r_dir = sim_settings$r_dir, N = sim_settings$N)
+      setTxtProgressBar(pb,jj)
+    }
+    close(pb)
+    results <- episim_data_ens
+
+  }
+
+
+  return(results)
+}
---
+++ code_to_test_functionality/test_file.R
@@ -0,0 +1,99 @@
+library(EpiControl)
+library(VGAM)
+library(parallel)
+library(pbapply)
+library(zoo)  # For rolling sum operations
+library(dplyr)
+library(ggplot2)
+
+Epi_pars <- data.frame(
+  Pathogen = c("COVID-19", "Ebola"),
+  R0 = c(2.2, 2.5),
+  gen_time = c(6.5, 15.0),
+  gen_time_var = c(2.1, 2.1),
+  CFR = c(0.0132, 0.5),
+  mortality_mean = c(10.0, 10.0),
+  mortality_var = c(1.1, 1.1)
+)
+
+Noise_pars <- data.frame(
+  repd_mean = 10.5,  # Reporting delay mean
+  del_disp = 5.0,    # Reporting delay variance
+  ur_mean = 0.3,     # Under-reporting mean
+  ur_beta_a = 50.0   # Beta distribution alpha for under-reporting
+)
+
+Action_space <- data.frame(
+  NPI = c("No restrictions", "Lockdown"),
+  R_coeff = c(1.0, 0.3),
+  R_beta_a = c(0.0, 5.0),
+  cost_of_NPI = c(0.0, 0.15)
+)
+
+sim_settings <- list(
+  ndays = 83 * 7, #simulation length
+  start_day = 1,
+  N = 1e7, # population size
+  I0 = 10, # initial infections
+  C_target = 5000, #target cases
+  C_target_pen = C_target*1.5, #overshoot penalty threshold
+  R_target = 1.0,
+  D_target = 12, #one way to get peaks at 400 is to increase this to 15
+  D_target_pen = 50, #max death
+  alpha = 0*1.3/C_target, #~proportional gain (regulates error in cases) covid
+  #alpha = 3.25/C_target #~proportional gain (regulates error in cases) ebola
+  alpha_d = 1.3/D_target,
+  ovp = 0*5.0, #overshoot penalty
+  dovp = 10.0, #death overshoot penalty
+  gamma = 0.95, #discounting factor
+  n_ens = 20L, #MC assembly size for 4
+  sim_ens = 100L, #assembly size for full simulation
+  rf = 21L, #days 14
+  R_est_wind = 5L, #rf-2 #window for R estimation
+  susceptibles = 0,
+  pred_days = 28L,
+  r_trans_steep = 1.5,  # Growth rate
+  r_trans_len = 7,  # Number of days for the transition
+  t0 = r_trans_len  / 2, # Midpoint of the transition
+  pathogen = 1,
+  susceptibles = 0,
+  delay = 1,
+  ur = 1,
+  r_dir = 2
+)
+
+episettings <- list(
+  sim_function = Epi_MPC_run_est_D,
+  projection_function = Epi_pred_wd,
+  reward_function = reward_fun_wd,
+  R_estimator = R_estim,
+  noise_par = Noise_pars,
+  epi_par = Epi_pars,
+  actions = Action_space,
+  sim_settings = sim_settings,
+  parallel = TRUE
+)
+
+column_names <- c("days", "sim_id", "I", "Lambda", "C", "Lambda_C", "S", "Deaths", "Re", "Rew", "Rest", "R0est", "policy", "R_coeff")
+episim_data <- data.frame(matrix(0, nrow = ndays, ncol = length(column_names)))
+colnames(episim_data) <- column_names
+
+episim_data$policy <- rep(1, ndays)
+episim_data$days <- 1:ndays
+episim_data[1, ] <- c(1, 1, I0, I0, Noise_pars$ur_mean * I0, Noise_pars$ur_mean * I0, N - I0, 0, Epi_pars[1, "R0"], Epi_pars[1, "R0"], 1, 1, 1, 1)
+
+episim_data_ens <- replicate(sim_ens, episim_data, simplify = FALSE)
+
+for (ii in 1:sim_ens) {
+  episim_data_ens[[ii]]$sim_id <- rep(ii, ndays)
+}
+
+
+cores=detectCores()-1
+cl <- makeCluster(cores)
+
+clusterExport(cl, ls())
+
+results <- epicontrol(episim_data_ens, episettings)
+
+
